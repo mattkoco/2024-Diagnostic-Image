ee6c5256a9b658a293ef6f0f7bdc783486e4259b
Matthew <mattkoco@icloud.com>
2024-08-08 12:07:26 -0400

    Update scorebot.sh

--- scorebot.sh
+++ scorebot.sh
@@ -51,3 +51,4 @@ check_text_exists "/etc/sysctl.conf" "net.ipv4.conf.all.log_martians = 1" "Enabl
 
 check_text_not_exists "/etc/group" "Cole" "Removed unauthorized user Cole"
 check_text_not_exists "/etc/shadow" "Connor" "Removed unauthorized user Connor"
+check_text_exists "/etc/group" "field_ops:x:1016:Coyne,Ryan" "Added user Ryan to group Field Ops"
