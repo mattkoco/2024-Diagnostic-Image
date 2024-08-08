b0e864e97c5a2d7208d13d5c9dd9c9d708988517
Matthew <mattkoco@icloud.com>
2024-08-08 12:51:07 -0400

    Update scorebot.sh

--- scorebot.sh
+++ scorebot.sh
@@ -84,3 +84,5 @@ check_file_deleted "/media/funnymonkey/awesomemusic.APE" "Deleted unwanted .APE 
 
 check_file_deleted "/bin/nmap" "Removed unauthorized software 'nmap'"
 check_file_deleted "/bin/fcrackzip" "Removed unauthorized software 'fcrackzip'"
+
+check_text_not_exists "/etc/bash.bashrc" "command apt" "Removed malicious alias 'apt update'"
