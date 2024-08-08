2462265ca02f273b4e353dfa837c116485298401
Matthew <mattkoco@icloud.com>
2024-08-08 12:07:57 -0400

    Update scorebot.sh

--- scorebot.sh
+++ scorebot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Please give around ~5 minutes for the raw to update
 # Scorebot for 2024 Diag
+echo " "
+echo " "
 echo "NOTE: Please allow up to 5 minutes for scorebot updates & injects."
 echo "Scorebot version: v1"
 echo "Injects: NO"
